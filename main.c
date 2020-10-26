c6185a591dc1f8e0015f83a49ba54d9163fc27dc
Mizuha Yayoi <poko.peko.tomo@icloud.com>
2020-10-26 20:32:38 +0900

    Initial

---
+++ main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+    int a,b;
+    float c,d;
+    a=1+2+3;
+    b=10-2-4;
+    c=1.2*2.4;
+    d=(float)10/20;
+    printf("a= %d b=%d c= %1.2f d= %1.1f\n",a,b,c,d);
+    return 0;
+}
